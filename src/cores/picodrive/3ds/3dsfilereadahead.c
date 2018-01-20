c4cc733b9bd3732ced2767911860d58e7ffae123
bubble2k16 <bubble2k16@gmail.com>
2018-01-20 23:17:16 +0800

    Fix read-ahead bug. Now small ISO games can work.

--- src/cores/picodrive/3ds/3dsfilereadahead.c
+++ src/cores/picodrive/3ds/3dsfilereadahead.c
@@ -1,6 +1,7 @@
 
 #include "stdio.h"
 #include "3dsfilereadahead.h"
+#include "3dsdbg.h"
 
 extern void debugWait();
 
@@ -11,7 +12,7 @@ extern void debugWait();
 void read_ahead_init(cd_read_ahead_struct *cd_read_ahead)
 {
   cd_read_ahead->fptr = NULL;
-  cd_read_ahead->seek_pos = 0xffffffff;
+  cd_read_ahead->seek_pos = 0;
   cd_read_ahead->buffer_pos = 0;
   cd_read_ahead->buffer_length = 0;
   memset(cd_read_ahead->buffer, 0, CD_READ_AHEAD_BUFFER_SIZE);
@@ -48,39 +49,25 @@ void read_ahead_fseek(cd_read_ahead_struct *cd_read_ahead, FILE *fp, int pos, in
     return;
 
   fseek(fp, pos, origin);
+  pos = ftell(fp);
 
   if (cd_read_ahead->fptr != fp)
   {
     cd_read_ahead->fptr = fp;
-
-    if (origin == SEEK_SET)
-      cd_read_ahead->seek_pos = pos;
-    else 
-      cd_read_ahead->seek_pos = 0x7fffffff;
-
+    cd_read_ahead->seek_pos = pos;
     cd_read_ahead->buffer_pos = 0;
     cd_read_ahead->buffer_length = 0;
   }
   else
   {
-    if (origin == SEEK_SET)
+    if (cd_read_ahead->seek_pos <= pos && 
+      pos < (cd_read_ahead->seek_pos + cd_read_ahead->buffer_length))
     {
-      if (cd_read_ahead->seek_pos != 0x7fffffff &&
-        cd_read_ahead->seek_pos <= pos && 
-        pos < (cd_read_ahead->seek_pos + cd_read_ahead->buffer_length))
-      {
-        cd_read_ahead->buffer_pos = pos - cd_read_ahead->seek_pos;
-      }
-      else
-      {
-        cd_read_ahead->seek_pos = pos;
-        cd_read_ahead->buffer_pos = 0;
-        cd_read_ahead->buffer_length = 0;
-      }
+      cd_read_ahead->buffer_pos = pos - cd_read_ahead->seek_pos;
     }
     else
     {
-      cd_read_ahead->seek_pos = 0xffffffff;
+      cd_read_ahead->seek_pos = pos;
       cd_read_ahead->buffer_pos = 0;
       cd_read_ahead->buffer_length = 0;
     }
@@ -96,8 +83,6 @@ void read_ahead_fseek(cd_read_ahead_struct *cd_read_ahead, FILE *fp, int pos, in
 int read_ahead_fread(cd_read_ahead_struct *cd_read_ahead, void *dest_buffer, int size, FILE *fp) 
 {
   if (fp == NULL)
-    return 0;
-  if (cd_read_ahead->seek_pos == 0xffffffff)
     return 0;
   
   int total_size = size;  
